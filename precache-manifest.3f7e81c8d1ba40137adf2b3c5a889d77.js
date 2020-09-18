d320f20309c10ed113613ae32b65eb1beef4878f
addygenerale <sammcaddy@gmail.com>
2020-09-18 07:11:42 +0000

    Updates

---
+++ precache-manifest.3f7e81c8d1ba40137adf2b3c5a889d77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104c1bf839316771f975c87c60a78d6f",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "f60f9bf4a19f4896062e",
+    "url": "/covid-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4af5fcf38d56a1b977fa",
+    "url": "/covid-tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f60f9bf4a19f4896062e",
+    "url": "/covid-tracker/static/js/2.ba2f7466.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-tracker/static/js/2.ba2f7466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af5fcf38d56a1b977fa",
+    "url": "/covid-tracker/static/js/main.34b5810e.chunk.js"
+  },
+  {
+    "revision": "e22acc657e89e90552a5",
+    "url": "/covid-tracker/static/js/runtime-main.fa5649eb.js"
+  }
+]);
