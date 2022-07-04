9ecaace57654987e878933577c4dc2eaf8c3fb31
addygeneral <sammcaddy@gmail.com>
2022-07-04 13:07:40 +0000

    Updates

---
+++ precache-manifest.1ade010a9c16e1e8185b88bdc5b93562.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd12de8e8bacad0247a84daaa4cf7588",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "48c80e76b391d251a5da",
+    "url": "/covid-tracker/static/css/2.1ebb0010.chunk.css"
+  },
+  {
+    "revision": "f3ba36296710092561be",
+    "url": "/covid-tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "48c80e76b391d251a5da",
+    "url": "/covid-tracker/static/js/2.3b01c69b.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/covid-tracker/static/js/2.3b01c69b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ba36296710092561be",
+    "url": "/covid-tracker/static/js/main.bfb8355e.chunk.js"
+  },
+  {
+    "revision": "67f2510ad273685b57cb",
+    "url": "/covid-tracker/static/js/runtime-main.25272686.js"
+  }
+]);
